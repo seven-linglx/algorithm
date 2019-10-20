9921e0c99f47d6494342af6acfdeb39c4dcc4855
seven-linglx <linglx@foxmail.com>
2019-10-20 14:51:35 +0100

    add:添加“寻找两个有序数组的中位数解法”

---
+++ leetcode/find_median_in_sorted_array.py
@@ -0,0 +1,8 @@
+"""
+给定两个大小为 m 和 n 的有序数组 nums1 和 nums2。
+请你找出这两个有序数组的中位数，并且要求算法的时间复杂度为 O(log(m + n))。
+"""
+
+
+def find_median_in_sorted_array(first: list, second: list) -> float:
+    """main function"""
