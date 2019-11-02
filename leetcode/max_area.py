c8c49f289850329b9eb1482d17ee582cac3908b8
seven-linglx <linglx@foxmail.com>
2019-11-02 11:08:47 +0100

    update:更新最大面积题解

--- leetcode/max_area.py
+++ leetcode/max_area.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-
 """
 给出一个整数序列，找出其最大面积
 """
@@ -8,4 +7,32 @@
 
 class Solution:
     def max_area(self, heights: list) -> int:
-        pass
+        area = 0
+        numbers = len(heights)
+        for i in range(numbers):
+            for j in range(i, numbers):
+                temp = (j - i) * min(heights[i], heights[j])
+                if temp > area:
+                    area = temp
+        return area
+
+
+class Solution2:
+    def max_area(self, heights: list) -> int:
+        area = 0
+        end = len(heights) - 1
+        start = 0
+        while start < end:
+            area = max((end - start) * min(heights[start], heights[end]), area)
+            if heights[start] < heights[end]:
+                start += 1
+            else:
+                end -= 1
+        return area
+
+
+def test():
+    solu = Solution2()
+    assert solu.max_area([2, 3, 4, 5]) == 6
+    assert solu.max_area([3, 7, 4, 5]) == 10
+    assert solu.max_area([1, 9, 8, 6, 2, 5, 4, 8, 3, 7]) == 56
